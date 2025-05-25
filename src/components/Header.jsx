de1d85351a9c002874a70ae7a74877a70c17edcf
riconect9 <ricardomobilidadesp@gmail.com>
2025-05-25 03:41:11 -0300

    Criação do componente Header. jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
+      <h1>Conect Client</h1>
+    </header>
+  );
+};
+
+export default Header;
