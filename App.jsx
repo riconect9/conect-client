9a66375db192a9bbb3b58d9ef94fb1cb194c1fe6
riconect9 <ricardomobilidadesp@gmail.com>
2025-05-25 04:14:41 -0300

    Adcionado o componente Header e ajustado o return do app.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
 import TelaExplorar from './components/TelaExplorar';
 export default function App() {
- return (<BrowserRouter><Routes><Route path='/explorar' element={<TelaExplorar />} /></Routes></BrowserRouter>); }
+return (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/explorar" element={<TelaExplorar />} />
+    </Routes>
+  </BrowserRouter>
+);
